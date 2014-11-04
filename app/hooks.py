7651e60f11a78fe93f8491a002ad1cafa442a5d0
Davis Robertson <davis@daviskr.com>
2014-11-04 14:24:23 -0600

    Fixing set_access_origin hook
    set_access_origin hook now actually responds correctly for CORS. It now
    gets the ORIGIN header instead of the host header.

--- app/hooks.py
+++ app/hooks.py
@@ -2,9 +2,9 @@ from app import access_origin_domains
 
 
 def set_access_origin(req, resp, resource):
-    host = req.host
-    if host in access_origin_domains:
-        resp.set_header("Access-Control-Allow-Origin", host)
+    orig = req.get_header("ORIGIN")
+    if orig in access_origin_domains:
+        resp.set_header("Access-Control-Allow-Origin", orig)
 
 
 # TODO: Implement this
