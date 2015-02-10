9da3da4b6e40196e945257fd33c508a67cf1ec11
Davis Robertson <davis@daviskr.com>
2015-02-10 12:16:02 -0600

    Adding More Columns
    Adding various columns to the models. I will have hook this up to the
    routing.

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class Student(Base):
     first_name = Column(String(50))
     last_name = Column(String(50))
     home_room_teacher_id = Column(String(30), ForeignKey("teacher.id"))
+    inactive = Column(Boolean())
 
     schedules = relationship("Schedule", backref="student")
 
@@ -20,12 +21,14 @@ class Student(Base):
         self.first_name = first_name
         self.last_name = last_name
         self.home_room_teacher_id = home_room_teacher_id
+        self.inactive = False
 
     def to_dict(self):
         return {"id": self.id,
                 "first_name": self.first_name,
                 "last_name": self.last_name,
-                "home_room_teacher_id": self.home_room_teacher_id}
+                "home_room_teacher_id": self.home_room_teacher_id,
+                "inactive": self.inactive}
 
     def __repr__(self):
         return "<Student %r>" % self.id
@@ -38,6 +41,7 @@ class Teacher(Base):
     first_name = Column(String(50))
     last_name = Column(String(50))
     room_id = Column(String(6))
+    data_manager = Column(Boolean())
 
     home_room_students = relationship("Student", backref="teacher")
     schedules = relationship("Schedule", backref="teacher")
@@ -49,6 +53,7 @@ class Teacher(Base):
         self.first_name = first_name
         self.last_name = last_name
         self.room_id = room_id
+        self.data_manager = False
 
     def to_dict(self):
         return {"id": self.id,
@@ -66,18 +71,21 @@ class Schedule(Base):
     student_id = Column(Integer, ForeignKey("student.id"), primary_key=True)
     teacher_id = Column(String(30), ForeignKey("teacher.id"), primary_key=True)
     date = Column(Date, primary_key=True)
+    comment = Column(String(256))
     absent = Column(Boolean)
 
     def __init__(self, student_id, teacher_id, date):
         self.student_id = student_id
         self.teacher_id = teacher_id
         self.date = date
+        self.comment = "(NONE)"
         self.absent = False
 
     def to_dict(self):
         return {"student_id": self.student_id,
                 "teacher_id": self.teacher_id,
                 "date": str(self.date),
+                "comment": self.comment,
                 "absent": self.absent}
 
     def __repr__(self):
